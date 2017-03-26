eaf07458e3598268cd46a9f811436bb43eb56aa9
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-03-26 14:29:14 +0700

    add componen TMyConnection on datamodule

--- uDm.dfm
+++ uDm.dfm
@@ -1,5 +1,10 @@
 object Dm: TDm
   OldCreateOrder = False
+  OnCreate = DataModuleCreate
   Height = 352
   Width = 455
+  object Con: TMyConnection
+    Left = 16
+    Top = 8
+  end
 end
--- uDm.pas
+++ uDm.pas
@@ -7,11 +7,14 @@ uses
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
   Data.DB, FireDAC.Comp.Client, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
-  FireDAC.Stan.ExprFuncs;
+  FireDAC.Stan.ExprFuncs, DBAccess, MyAccess;
 
 type
   TDm = class(TDataModule)
+    Con: TMyConnection;
+    procedure DataModuleCreate(Sender: TObject);
   private
+    procedure ConnectToDb(AHost, ADb, AUser, APassword: string);
     { Private declarations }
   public
     { Public declarations }
@@ -25,5 +28,19 @@ implementation
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
+
+procedure TDm.ConnectToDb(AHost, ADb, AUser, APassword: string);
+begin
+  con.Server := AHost;
+  con.Database := ADb;
+  con.Username := AUser;
+  Con.Password := APassword;
+  con.Open;
+end;
+
+procedure TDm.DataModuleCreate(Sender: TObject);
+begin
+  ConnectToDb('localhost', 'klinik', 'root', 'server');
+end;
 
 end.
