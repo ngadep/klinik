1ce87d8b34e3a00dfb3eafc30e04faeef9133ebf
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-03-27 22:23:59 +0700

    menggunakan skin bawaan dari delphi

--- SQLITE/klinik.dpr
+++ SQLITE/klinik.dpr
@@ -3,13 +3,16 @@ program klinik;
 uses
   Vcl.Forms,
   uPasien in 'uPasien.pas' {FrmPasien},
-  uDm in 'uDm.pas' {Dm: TDataModule};
+  uDm in 'uDm.pas' {Dm: TDataModule},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 Blue');
   Application.CreateForm(TDm, Dm);
   Application.CreateForm(TFrmPasien, FrmPasien);
   Application.Run;
